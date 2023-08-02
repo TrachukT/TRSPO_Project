1659cc0eac8483463676507c0e0ac96147016e98
Trachuk Tetiana <tankatrachuk@gmail.com>
2023-08-02 15:48:41 +0300

    added cosmosdb connection

--- TFSport.API/TFSport.API/Program.cs
+++ TFSport.API/TFSport.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCosmosRepository(options =>
+{
+	options.CosmosConnectionString = builder.Configuration.GetConnectionString("CosmosDb");
+	options.ContainerId = "users";
+	options.DatabaseId = "TFSport";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
