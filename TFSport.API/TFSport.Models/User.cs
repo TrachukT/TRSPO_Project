7d14f6c909ac419b9882690946f34e5cfa76c516
Daria Reu <dominica.diem@gmail.com>
2023-08-03 15:16:32 +0300

    user entity, service and controller

---
+++ TFSport.API/TFSport.Models/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.CosmosRepository.Attributes;
+using Newtonsoft.Json;
+
+namespace TFSport.Models
+{
+    [PartitionKeyPath("/partitionKey")]
+    public class User : BaseModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("surname")]
+        public string Surname { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("userRole")]
+        public UserRoles UserRole { get; set; }
+
+        [JsonIgnore]
+        public static string ContainerName { get; set; }
+    }
+}
---
+++ TFSport.API/TFSport.Models/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace TFSport.Models
+{
+    public enum UserRoles
+    {
+        SuperAdmin,
+        Author,
+        User
+    }
+}
