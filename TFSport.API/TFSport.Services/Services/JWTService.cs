3be06106f11cd7110af9605c62da8ad97ceb020a
Daria Reu <dominica.diem@gmail.com>
2023-08-22 12:03:13 +0300

    visual studio is an awesome software

--- TFSport.API/TFSport.Services/Services/JWTService.cs
+++ TFSport.API/TFSport.Services/Services/JWTService.cs
@@ -4,7 +4,6 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using TFSport.Models;
 using TFSport.Services.Interfaces;
 
 namespace TFSport.Services.Services
--- TFSport.API/TFSport.Services/Services/UserService.cs
+++ TFSport.API/TFSport.Services/Services/UserService.cs
@@ -63,40 +63,61 @@ namespace TFSport.Services.Services
 
 		public async Task<string> ValidateCredentialsAsync(string email, string password)
 		{
-			var user = await _userRepository.GetAsync(x => x.Email == email).FirstOrDefaultAsync();
-			if (user == null)
-			{
-				throw new ArgumentException(ErrorMessages.InvalidCredentials);
-			}
+			try
+            {
+                var user = await _userRepository.GetAsync(x => x.Email == email).FirstOrDefaultAsync();
+                if (user == null)
+                {
+                    throw new CustomException(ErrorMessages.InvalidCredentials);
+                }
 
-			var passwordHasher = new PasswordHasher();
-			var result = passwordHasher.VerifyHashedPassword(user.Password, password);
+                var passwordHasher = new PasswordHasher();
+                var result = passwordHasher.VerifyHashedPassword(user.Password, password);
 
-			if (result != PasswordVerificationResult.Success)
-			{
-				throw new ArgumentException(ErrorMessages.InvalidCredentials);
-			}
+                if (result != PasswordVerificationResult.Success)
+                {
+                    throw new CustomException(ErrorMessages.InvalidCredentials);
+                }
 
-			return user.Id;
-		}
+                return user.Id;
+            }
+            catch (ArgumentException arg)
+            {
+                throw new CustomException(arg.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex.Message);
+            }
+        }
 
 		public async Task RegisterUser(User user)
 		{
-			try
-			{
+            try
+            {
                 var checkUser = await _userRepository.GetAsync(x => x.Email == user.Email).FirstOrDefaultAsync();
                 if (checkUser != null)
-				{
+                {
                     throw new CustomException(ErrorMessages.EmailIsRegistered);
                 }
 
                 var hash = new PasswordHasher();
                 user.Password = hash.HashPassword(user.Password);
 
-			await _userRepository.CreateAsync(user, default);
-			await _emailService.EmailVerification(user.Email, user.VerificationToken);
-			_logger.LogInformation("User with id {id} was created", user.Id);
-		}
+                await _userRepository.CreateAsync(user, default);
+                await _emailService.EmailVerification(user.Email, user.VerificationToken);
+                _logger.LogInformation("User with id {id} was created", user.Id);
+            }
+            catch (ArgumentException arg)
+            {
+                throw new CustomException(arg.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex.Message);
+            }
+
+        }
 
 		public async Task ForgotPassword(string email)
 		{
@@ -122,37 +143,56 @@ namespace TFSport.Services.Services
 
 		public async Task RestorePassword(string token, string password)
 		{
-			try
-			{
+            try
+            {
                 var user = await _userRepository.GetAsync(x => x.VerificationToken == token).FirstOrDefaultAsync();
                 if (user == null)
-				{
+                {
                     throw new CustomException(ErrorMessages.NotValidLink);
                 }
 
-			user.VerificationToken = Guid.NewGuid().ToString();
-			var hash = new PasswordHasher();
-			user.Password = hash.HashPassword(password);
+                user.VerificationToken = Guid.NewGuid().ToString();
+                var hash = new PasswordHasher();
+                user.Password = hash.HashPassword(password);
 
-			await _userRepository.UpdateAsync(user, default);
-			_logger.LogInformation("Password for user with id {id} was updated", user.Id);
-		}
+                await _userRepository.UpdateAsync(user, default);
+                _logger.LogInformation("Password for user with id {id} was updated", user.Id);
+
+            }
+            catch (ArgumentException arg)
+            {
+                throw new CustomException(arg.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex.Message);
+            }
+        }
 
 		public async Task EmailVerification(string verificationToken)
 		{
-			try
-			{
+            try
+            {
                 var user = await _userRepository.GetAsync(x => x.VerificationToken == verificationToken).FirstOrDefaultAsync();
                 if (user == null)
                 {
                     throw new CustomException(ErrorMessages.NotValidLink);
                 }
 
-			user.VerificationToken = Guid.NewGuid().ToString();
-			user.EmailVerified = true;
-			await _userRepository.UpdateAsync(user, default);
-			_logger.LogInformation("User with id {id} successfully verified email", user.Id);
-		}
+                user.VerificationToken = Guid.NewGuid().ToString();
+                user.EmailVerified = true;
+                await _userRepository.UpdateAsync(user, default);
+                _logger.LogInformation("User with id {id} successfully verified email", user.Id);
+            }
+            catch (ArgumentException arg)
+            {
+                throw new CustomException(arg.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex.Message);
+            }
+        }
 
 		public async Task CreateSuperAdminUser()
 		{
@@ -174,11 +214,16 @@ namespace TFSport.Services.Services
                         VerificationToken = Guid.NewGuid().ToString()
                     };
 
-				superAdmin.PartitionKey = superAdmin.Id;
-				await RegisterUser(superAdmin);
-				_logger.LogInformation("Super admin created");
-			}
-		}
+                    superAdmin.PartitionKey = superAdmin.Id;
+                    await RegisterUser(superAdmin);
+                    _logger.LogInformation("Super admin created");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex.Message);
+            }
+        }
 
 		public async Task<List<User>> GetAllUsers()
 		{
@@ -203,19 +248,28 @@ namespace TFSport.Services.Services
                     throw new CustomException($"Invalid role specified: {newUserRole}.");
                 }
 
-			var user = await _userRepository.GetAsync(userId);
-			if (user != null)
-			{
-				user.UserRole = (UserRoles)Enum.Parse(typeof(UserRoles), newUserRole, ignoreCase: true);
-				await _userRepository.UpdateAsync(user, default);
-				_logger.LogInformation("User with id {id} now has role {role}", user.Id, user.UserRole);
-				return true;
-			}
-			else
-			{
-				throw new Exception(ErrorMessages.UserNotFound);
-			}
-		}
+                var user = await _userRepository.GetAsync(userId);
+                if (user != null)
+                {
+                    user.UserRole = (UserRoles)Enum.Parse(typeof(UserRoles), newUserRole, ignoreCase: true);
+                    await _userRepository.UpdateAsync(user, default);
+                    _logger.LogInformation("User with id {id} now has role {role}", user.Id, user.UserRole);
+                    return true;
+                }
+                else
+                {
+                    throw new CustomException($"User with ID {userId} not found.");
+                }
+            }
+            catch (ArgumentException arg)
+            {
+                throw new CustomException(arg.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ex.Message);
+            }
+        }
 
 		public async Task<User> GetUserById(string id)
 		{
