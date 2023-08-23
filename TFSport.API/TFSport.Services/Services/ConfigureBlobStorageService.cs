766c0a326021b103b9110bdb38735ed4657fb602
Daria Reu <dominica.diem@gmail.com>
2023-08-23 22:19:38 +0300

    #423 Create blob storage service

---
+++ TFSport.API/TFSport.Services/Services/ConfigureBlobStorageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TFSport.Models;
+
+namespace TFSport.Services.Services
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBlobStorageService(this IServiceCollection services, string connectionString)
+        {
+            services.Configure<BlobStorageOptions>(options => options.ConnectionString = connectionString);
+
+            return services;
+        }
+    }
+}
