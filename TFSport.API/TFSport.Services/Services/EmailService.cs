4db6e16a2d7c79e30b4796065daf287caab18fe5
Таня Трачук <tankatrachuk@gmail.com>
2023-09-09 20:50:14 +0300

    #840 added method for sending mail message after publishing post

--- TFSport.API/TFSport.Services/Services/EmailService.cs
+++ TFSport.API/TFSport.Services/Services/EmailService.cs
@@ -22,7 +22,7 @@ namespace TFSport.Services.Services
 		{
 			var link = _emailSettings.EmailUrl + verificationToken;
 			var content = $"To complete registration you need to verificate email.To do this click the link below:\n{link}";
-			await CreateMessage(email, content);
+			await CreateMessage(email, content, "Email Verification");
 			_logger.LogInformation("Email with link for email verification has been send on email {email}", email);
 		}
 
@@ -30,20 +30,27 @@ namespace TFSport.Services.Services
 		{
 			var link = _emailSettings.PasswordUrl + verificationToken;
 			var content = $"To restore password click the link below: \n{link}";
-			await CreateMessage(email, content);
+			await CreateMessage(email, content, "Restore Password");
 			_logger.LogInformation("Email with link for restoring password has been send on email {email}", email);
 		}
-		public async Task CreateMessage(string email, string content)
+		public async Task CreateMessage(string email, string content,string subject)
 		{
 			var client = new SendGridClient(_emailSettings.ApiKey);
 			var msg = new SendGridMessage()
 			{
 				From = new EmailAddress(_emailSettings.SenderEmail, _emailSettings.SenderName),
-				Subject = "Email Verification",
+				Subject = subject,
 				PlainTextContent = content
 			};
 			msg.AddTo(new EmailAddress(email));
 			await client.SendEmailAsync(msg);
 		}
-	}
+
+        public async Task ArticleIsPublished(string email, string articleName)
+        {
+			var content = $"Congratulations! Your article \"{articleName}\" was published.";
+			await CreateMessage(email, content, "Article is Published");
+            _logger.LogInformation("Email about successfull publish of article has been send on email {email}", email);
+        }
+    }
 }
